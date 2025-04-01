34bdd90e133c123eed502b4c7d0247e067549fa6
Lucas Generozo <lucasgenerozo@outlook.com>
2025-03-31 23:34:24 -0300

    feat: iniciado implementação do datasource

--- src/Models/Infrastructure/PDO/PDODataSource.php
+++ src/Models/Infrastructure/PDO/PDODataSource.php
@@ -1,17 +1,37 @@
 <?php
 namespace Lucas\Tcc\Models\Infrastructure\PDO;
 
-use DataBase;
-use Lucas\Tcc\Models\Domain\DataSource\ReadableDataSource;
+use Lucas\Tcc\Models\Domain\DataSource\DataSource;
 use PDO;
 
-class PDODataSource implements ReadableDataSource
+class PDODataSource implements DataSource
 {
     public function __construct(
         private string $name,
         private ?array $with,
         private PDO $pdo
     ) {}
+
+    public function listAll(): ?array
+    {
+        $stmt = $this->pdo->prepare("
+            SELECT *
+            FROM   $this->name
+        ");
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function listByCallback(callable $callback): ?array
+    {
+
+    }
+
+    public function findBy(string $label, mixed $value): mixed
+    {
+
+    }
 
 
 }
